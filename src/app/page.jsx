d19991af3024bc1288257490b1b8fa8c60f08cd4
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2023-09-01 09:27:06 +0600

    folder wise page create

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Hello Next Js</h1>
+    </div>
+  )
+}
