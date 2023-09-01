3392c51a16e6d0b36b7ba44f7249710cfdd34892
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2023-09-01 11:03:25 +0600

    images explained

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Hero from 'public/hero.png';
 
 export default function Home() {
   return (
     <div className="">
-      <h1>Hello Next Js</h1>
+      <Image width={500} height={500} src="https://images.pexels.com/photos/18111088/pexels-photo-18111088/free-photo-of-beach-vacation-people-sand.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt='' className={styles.img}/>
     </div>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './page.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
@@ -7,7 +8,12 @@ const Footer = () => {
       <div className={styles.footer}>
         @2023 Lamamia. All rights reserved.
       </div>
-      <div className=""></div>
+      <div className={styles.social}>
+          <Image className={styles.icon} src='/1.png' width={15} height={15} alt='facbook'/>
+          <Image className={styles.icon} src='/2.png' width={15} height={15} alt='instagram'/>
+          <Image className={styles.icon} src='/3.png' width={15} height={15} alt='twitter'/>
+          <Image className={styles.icon} src='/4.png' width={15} height={15} alt='youtube'/>
+      </div>
     </div>
   )
 }
